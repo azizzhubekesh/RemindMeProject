dc3004b2e0326508688d8eb06cce799176ac5a56
azizzhubekesh <aziz.zhubekesh@gmail.com>
2016-03-25 22:28:13 +0600

    Create base project structure

---
+++ RemindMe/app/src/main/java/com/azizzh/remindmeproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.azizzh.remindmeproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by azizz on 25-Mar-16.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
